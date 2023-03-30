fa27622ac1eb33e0155f920b80ca790776ab7f04
Dmytro Hirich <diman12345677@gmail.com>
2023-03-30 23:03:27 +0300

    set search params for movies list

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -15,7 +15,7 @@ const Movies = () => {
     Authorization: token,
     title: search,
     search: actorSearch,
-    sort: sortingBy,
+    sort: sortingBy
   });
 
   const debouncedSearchHandler = useDebounce((value: string) => {
@@ -43,76 +43,51 @@ const Movies = () => {
   useEffect(() => {}, [search, actorSearch]);
 
   if (isLoading) return <div>loading</div>;
-  // if (!data || !data.data || !data.data.length) return <div className="font-bold text-center text-2xl">there is no results. add some movies or change search params</div>;
-
-  return (
-    <div className="flex flex-col w-full">
-      <div className="flex">
-        <div className="p-4">
-          <TextField
-            id="outlined-basic"
-            label="search by title"
-            variant="outlined"
-            onChange={(e) => searchHandler(e)}
-            type="text"
-            required
-          />
+  
+    return (
+      <div className="flex flex-col w-full">
+        <div className="flex">
+          <div className="p-4">
+            <TextField
+              id="outlined-basic"
+              label="search by title"
+              variant="outlined"
+              onChange={(e) => searchHandler(e)}
+              type="text"
+              required
+            />
+          </div>
+          <div className="p-4">
+            <TextField
+              id="outlined-basic"
+              label="search by actor"
+              variant="outlined"
+              onChange={(e) => actorSearchHandler(e)}
+              type="text"
+              required
+            />
+          </div>
+          <div className="flex h-[50%] self-center gap-x-2">
+            sort by:
+            <Button variant="contained" size="small" onClick={() => setSortingBy('year')}>
+              <span className="text-white">{sortingBy === 'year' ? <span className="font-bold">year</span>: <span>year</span>}</span>
+            </Button>
+            <Button variant="contained" size="small" onClick={() => setSortingBy('title')}>
+              <span className="text-white">{sortingBy === 'title' ? <span className="font-bold">title</span>: <span>title</span>}</span>
+            </Button>
+          </div>
         </div>
-        <div className="p-4">
-          <TextField
-            id="outlined-basic"
-            label="search by actor"
-            variant="outlined"
-            onChange={(e) => actorSearchHandler(e)}
-            type="text"
-            required
-          />
-        </div>
-        <div className="flex h-[50%] self-center gap-x-2">
-          sort by:
-          <Button
-            variant="contained"
-            size="small"
-            onClick={() => setSortingBy("year")}
-          >
-            <span className="text-white">
-              {sortingBy === "year" ? (
-                <span className="font-bold">year</span>
-              ) : (
-                <span>year</span>
-              )}
-            </span>
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            onClick={() => setSortingBy("title")}
-          >
-            <span className="text-white">
-              {sortingBy === "title" ? (
-                <span className="font-bold">title</span>
-              ) : (
-                <span>title</span>
-              )}
-            </span>
-          </Button>
-        </div>
-      </div>
-      {data && data.data && data.data.length ? (
-        <div className="flex flex-wrap">
+        {
+          data && data.data && data.data.length ? <div className="flex flex-wrap">
           {data.data.map((movie) => {
             return (
               <Movie key={movie.id} movie={movie} actorSearch={actorSearch} />
             );
           })}
-        </div>
-      ) : (
-        <div className="font-bold text-center text-2xl">
-          there is no results. add some movies or change search params
-        </div>
-      )}
-    </div>
-  );
+        </div> : <div className="font-bold text-center text-2xl">there is no results. add some movies or change search params</div>
+        }
+      </div>
+    );
 };
 
 export default Movies;
